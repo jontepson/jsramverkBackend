f71d42754a63641948e44ff9b9287efcaa2abdc4
jontepson <ceccoham@gmail.com>
2021-09-17 12:32:40 +0100

    travis

--- db/database.js
+++ db/database.js
@@ -4,13 +4,23 @@
  "use strict";
 
 const mongo = require("mongodb").MongoClient;
-const config = require("./config.json");
 const collectionName = config.collection;
+let config;
+let username;
+let password;
 
 const database = {
     getDb: async function getDb () {
-        let dsn = `mongodb+srv://${config.username}:${config.password}@${config.cluster}.c7v9q.mongodb.net/${config.database}?retryWrites=true&w=majority`;
+        try {
+            config = require("./config.json");
+        } catch (e) {
+            console.log(e)
+        }
 
+        username = process.env.USERNAME || config.username;
+        password = process.env.PASSWORD || config.password;
+        let dsn = `mongodb+srv://${config.username}:${config.password}@$cluster0.c7v9q.mongodb.net/editor?retryWrites=true&w=majority`;
+        //let dsn = `mongodb+srv://${config.username}:${config.password}@${config.cluster}.c7v9q.mongodb.net/${config.database}?retryWrites=true&w=majority`;
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";
         }
